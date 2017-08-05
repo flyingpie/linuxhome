489e8c3b5923f7b54f8b11a0c5be2a4d5ea7a9d5
Marco van den Oever <marco@flyingpie.nl>
2017-08-05 11:27:48 +0200

    .linuxhome.sh and some more commands

---
+++ .linuxhome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Cleaning up home..."
+rm -fr .git .local .ssh .bashrc .profile
+
+echo "Init..."
+git init
+
+echo "Add remote..."
+git remote add origin https://github.com/FlyingPie/linuxhome.git
+
+echo "Fetch..."
+git fetch
+
+echo "Reset..."
+git reset origin/master --hard
