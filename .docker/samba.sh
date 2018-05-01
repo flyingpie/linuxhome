8392692ce5f49cade963e9941d946468e10c3fac
Marco van den Oever <marco@flyingpie.nl>
2018-05-01 10:40:26 +0200

    Samba

---
+++ .docker/samba.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker rm -f samba
+
+docker run --name samba -d \
+  -p 137:137 \
+  -p 139:139 \
+  -p 445:445 \
+  -v /:/data \
+  --restart always \
+  dperson/samba \
+    -n \
+    -s "Data;/data;yes;no"
+
+docker logs -f samba
+
